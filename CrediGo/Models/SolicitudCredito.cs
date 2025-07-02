d5612a3aa3019d50481551ed7aa6dc548c3be63f
Marcoastudilloo <marcoantonioastudillo125@gmail.com>
2025-07-02 12:50:46 -0600

    Se creo los modelos y endpoint para crear Solicitud de credito

---
+++ CrediGo/Models/SolicitudCredito.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class SolicitudCredito
+{
+    [Key]
+    public int Id_solicitud { get; set; }
+
+    public int Id_usuario { get; set; }
+    public int Id_cliente { get; set; }
+    public decimal Monto_solicitado { get; set; }
+    public int Plazo_meses { get; set; }
+    public string Motivo { get; set; }
+    public DateTime Fecha_solicitud { get; set; }
+    public int Id_estatus { get; set; }
+}

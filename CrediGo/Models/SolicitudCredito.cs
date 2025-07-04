c7843d06b221f51fd18eb4288b8ae3b21267eb64
Marcoastudilloo <marcoantonioastudillo125@gmail.com>
2025-07-04 13:43:38 -0600

    Ultima version para usuarios

--- CrediGo/Models/SolicitudCredito.cs
+++ CrediGo/Models/SolicitudCredito.cs
@@ -1,15 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class SolicitudCredito
+namespace CrediGo.Models
 {
-    [Key]
-    public int Id_solicitud { get; set; }
+    public class SolicitudCredito
+    {
+        [Key]
+        public int Id_solicitud { get; set; }
 
-    public int Id_usuario { get; set; }
-    public int Id_cliente { get; set; }
-    public decimal Monto_solicitado { get; set; }
-    public int Plazo_meses { get; set; }
-    public string Motivo { get; set; }
-    public DateTime Fecha_solicitud { get; set; }
-    public int Id_estatus { get; set; }
+        public int Id_usuario { get; set; }
+
+        [ForeignKey("Id_usuario")]
+        public Usuario Usuario { get; set; }  // Relación con Usuario
+
+        public int Id_cliente { get; set; }
+
+        [ForeignKey("Id_cliente")]
+        public Cliente Cliente { get; set; }  // Relación con Cliente
+
+        public decimal Monto_solicitado { get; set; }
+        public int Plazo_meses { get; set; }
+        public string Motivo { get; set; }
+        public DateTime Fecha_solicitud { get; set; }
+        public int Id_estatus { get; set; }
+    }
 }

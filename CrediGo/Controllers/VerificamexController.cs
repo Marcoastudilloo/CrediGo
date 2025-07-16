fbfee196b29e974d3fa3f55da3b8a1479ceee567
Marcoastudilloo <marcoantonioastudillo125@gmail.com>
2025-07-16 13:02:52 -0600

    Verificamex prueba 1

---
+++ CrediGo/Controllers/VerificamexController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using CrediGo.Services;
+using CrediGo.Models.Verificamex;
+
+namespace CrediGo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VerificamexController : ControllerBase
+    {
+        [HttpPost("verificar-curp")]
+        public async Task<IActionResult> VerificarCurp([FromBody] RenapoRequest request, [FromServices] VerificamexService verificamexService)
+        {
+            var resultado = await verificamexService.ValidarCurpAsync(request.curp);
+
+            if (resultado == null)
+                return BadRequest(new { mensaje = "CURP no válida o no encontrada en RENAPO" });
+
+            return Ok(new
+            {
+                mensaje = "CURP válida",
+                datos = resultado
+            });
+        }
+
+        [HttpGet("descargar-pdf/{curp}")]
+        public async Task<IActionResult> DescargarPdf(string curp, [FromServices] VerificamexService verificamexService)
+        {
+            var resultadoCompleto = await verificamexService.ValidarCurpConPdfAsync(curp);
+
+            if (resultadoCompleto == null || resultadoCompleto.data == null || string.IsNullOrEmpty(resultadoCompleto.data.pdf))
+                return NotFound(new { mensaje = "PDF no encontrado para la CURP especificada." });
+
+            try
+            {
+                byte[] pdfBytes = Convert.FromBase64String(resultadoCompleto.data.pdf);
+                return File(pdfBytes, "application/pdf", $"{curp}.pdf");
+            }
+            catch (FormatException)
+            {
+                return BadRequest(new { mensaje = "Error al decodificar el PDF." });
+            }
+        }
+
+    }
+}
---
+++ CrediGo/Models/Verificamex/VerificamexResponse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace CrediGo.Models.Verificamex
+{
+    public class VerificamexRenapoResponse
+    {
+        public VerificamexData data { get; set; }
+        public object meta { get; set; }
+    }
+
+    public class VerificamexData
+    {
+        public string @object { get; set; }
+        public CitizenInformation citizen { get; set; }
+
+        public string pdf { get; set; }
+    }
+
+    public class CitizenInformation
+    {
+        public string mensaje { get; set; }
+        public string codigo { get; set; }
+
+        public List<CitizenRecord> registros { get; set; }
+        public bool status { get; set; }
+    }
+
+    public class CitizenRecord
+    {
+        public string curp { get; set; }
+        public string nombres { get; set; }
+        public string primerApellido { get; set; }
+        public string segundoApellido { get; set; }
+        public string sexo { get; set; }
+        public string fechaNacimiento { get; set; }
+        public string nacionalidad { get; set; }
+        public string entidad { get; set; }
+        public string claveEntidad { get; set; }
+    }
+}
